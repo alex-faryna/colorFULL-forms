d5dc50683d9ad0ef786d027afa0a60989e6bf2b0
alex <109907113+alex-faryna@users.noreply.github.com>
2023-06-07 23:29:46 +0300

    routing

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,47 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import {Provider} from "react-redux";
 import store from './store/index';
+import {createBrowserRouter, createRoutesFromElements, redirect, Route, RouterProvider} from "react-router-dom";
 
+export class RoutesConfig {
+    public static root = '/';
+    public static any = '*';
+    public static tests = 'tests';
+    public static test = `${RoutesConfig.tests}/:testId`; // for unauthorized users too
+}
+
+
+const redirectFn = (to: string) => () => redirect(to);
+
+function A() {
+    return <span>A</span>
+}
+
+function B() {
+    return <span>B</span>
+}
+
+function C() {
+    return <span>C</span>
+}
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path={RoutesConfig.root} Component={App}>
+            <Route path={RoutesConfig.tests} Component={A} />
+            <Route path={RoutesConfig.test} Component={B}>
+                <Route path={RoutesConfig.any} loader={redirectFn("") }></Route>
+            </Route>
+            <Route path={RoutesConfig.root} loader={redirectFn(RoutesConfig.tests) } />
+            <Route path={RoutesConfig.any} loader={redirectFn(RoutesConfig.tests) } />
+        </Route>
+    )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
     <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
     </Provider>
 );
 
