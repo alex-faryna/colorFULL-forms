adb878cec53ae3f91474d1e595d6c36269d37139
alex <109907113+alex-faryna@users.noreply.github.com>
2023-06-08 01:03:23 +0300

    fixed layout

--- src/pages/tests-list.page.tsx
+++ src/pages/tests-list.page.tsx
@@ -1,4 +1,48 @@
 import styled from "@emotion/styled";
+import {cloneElement, Component, createRef, Fragment, useRef} from "react";
+import useResize from "../utils/resize";
+import {Flipped, Flipper} from "react-flip-toolkit";
+
+class FlipAnimated extends Component {
+    private ref = createRef<HTMLElement>();
+
+    getSnapshotBeforeUpdate() {
+        console.log('p');
+        if (this.ref.current) {
+            // first
+            return this.ref.current.getBoundingClientRect();
+        }
+        return null;
+    }
+
+    // below `snapshot` is whatever returned `getSnapshotBeforeUpdate`
+    componentDidUpdate(prevProps: any, prevState: any, snapshot: any) {
+        console.log('p');
+        if (this.ref.current) {
+            const first = snapshot;
+            // last
+            const last = this.ref.current.getBoundingClientRect();
+            // invert
+            const deltaX = last.left - first.left;
+            const deltaY = last.top - first.top;
+
+            this.ref.current.animate([
+                { transform: `translate(${-deltaX}px, ${-deltaY}px)` },
+                // play
+                { transform: 'translate(0,0)' },
+            ], {
+                duration: 300,
+                easing: 'ease-out',
+            });
+        }
+    }
+    render() {
+
+        console.log("bro");
+        // @ts-ignore
+        return this.props.children;
+    }
+}
 
 const ColumnContainer = styled.div`
   display: flex;
@@ -15,21 +59,35 @@ const Grid = styled.div`
   display: grid;
   gap: 12px;
   grid-template-columns: repeat( auto-fill, minmax(250px, 1fr) );
+  align-items: center;
 `;
 
 const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  align-items: center;
+`;
+
+const InnerCard = styled.div`
+  padding: 1rem;
   border: 1px solid #ccc;
   border-radius: 4px;
+  width: 100%;
+  height: 100%;
   max-width: 300px;
-  padding: 1rem;
-  height: 200px;
 `;
 
 function TestCard({ name }: { name: string }) {
-    return <Card>{ name }</Card>
+    return <Card>
+        <InnerCard>{ name }</InnerCard>
+    </Card>
 }
 
 function TestsListPage() {
+    const grid = useRef<HTMLDivElement>(null);
+    const width = useResize(grid, 150);
+
     const tests = [
         {
             id: 0,
@@ -54,7 +112,7 @@ function TestsListPage() {
     ]
 
     return <ColumnContainer>
-        <Grid>
+        <Grid ref={grid}>
             {
                 tests.map(test => <TestCard key={test.id} name={test.name} />)
             }
