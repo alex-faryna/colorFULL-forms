39ad5245b378fd0b6b3ace43146f3aa5b12e355e
alex <109907113+alex-faryna@users.noreply.github.com>
2023-06-13 18:37:29 +0300

    added questions typings

--- src/models/test.model.ts
+++ src/models/test.model.ts
@@ -1,17 +1,48 @@
 
 
-export type QuestionTypes = 'text' | 'radio' | 'checkbox' | 'number' | 'float' | 'select';
+export type QuestionTypes = 'text' | 'number' | 'select';
 // elect with possibility of ading new values
 // also list ? where you can add multiple values (kinda text but formatted better)
 
-export interface Question {
+export interface BaseQuestion {
     title: string;
     required?: boolean;
     type: QuestionTypes;
 }
 
-export interface Test {
+
+interface FullTextQuestion extends BaseQuestion{
+    type: 'text';
+    multiline?: boolean;
+    correctAnswers: string[];
+}
+
+export interface FullNumberQuestion extends BaseQuestion {
+    type: 'number';
+    integer?: boolean;
+    correctAnswers: number[];
+}
+
+export interface FullSelectQuestion extends BaseQuestion {
+    type: 'select';
+    multiple?: boolean; // for radio / checkboxes
+    other?: boolean;
+    choices: QuestionsChoice[];
+    correctAnswers: number[];
+}
+
+export interface QuestionsChoice {
+    id: number,
+    text: string;
+}
+
+type FullQuestion = FullTextQuestion | FullNumberQuestion | FullSelectQuestion;
+export type Question<T extends boolean = false> = Omit<FullQuestion, T extends false ? 'correctAnswers' : ''>;
+
+
+export interface Test<T extends boolean = false> {
     id: string;
     title: string;
-    questions: Question[];
+    withAnswers?: T; // if true, when we configure the test we add the answers, the answers are ALWAYS checked on the server
+    questions: Question<T>[];
 }
